5449f80bb41d318b8fa6eb2f06b74fab54c9ccb2
toshiya <arche.dsp@gmail.com>
2021-10-13 20:41:14 -0500

    Agregando configuración para hacer uso de MySQL como base de datos en settings-prod.py

--- Bedutravels/settings-prod.py
+++ Bedutravels/settings-prod.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'Bedutravels.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'toshiya$Bedutravels',
+        'USER': 'toshiya',
+        'PASSWORD': 'pythonsql',
+        'HOST': 'toshiya.mysql.pythonanywhere-services.com',
     }
 }
 
